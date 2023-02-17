c567893620cd6a8d87a8b7d195a299caa422b7ee
mikhail.gasin <mikhail.gasin@fors.ru>
2023-02-17 17:34:01 +0300

    Reworked frontend for handling server errors

--- src/main/frontend/src/API/EncryptingService.js
+++ src/main/frontend/src/API/EncryptingService.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 export default class EncryptingService {
-    static SERVER_ADDRESS = 'http://localhost:8080'
+    static SERVER_ADDRESS = 'http://localhost:8085'
+    // static SERVER_ADDRESS = process.env.BACKEND_URL
 
     static async encrypt(text) {
         const url = this.SERVER_ADDRESS + '/api/v1/encrypt'
